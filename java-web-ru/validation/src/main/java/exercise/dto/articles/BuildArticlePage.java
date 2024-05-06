c9b2a8918d507b7a78a6b8ea9bbf6d8bf05fdd78
@hexlet/cli <support@hexlet.io>
2024-05-06 06:10:45 +0300

    submit java-web-ru/validation

---
+++ java-web-ru/validation/src/main/java/exercise/dto/articles/BuildArticlePage.java
@@ -0,0 +1,23 @@
+package exercise.dto.articles;
+
+import io.javalin.validation.ValidationError;
+import java.util.Map;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+
+// BEGIN
+@AllArgsConstructor
+@Getter
+public class BuildArticlePage {
+    private String title;
+    private String content;
+    private Map<String, List<ValidationError<Object>>> errors;
+
+    public BuildArticlePage() {
+    }
+}
+
+// END
